bfc80e7a744ae11376e093935316b07815cb035b
Byungha Ko <rhqo123@gmail.com>
2024-10-18 19:23:56 +0900

    vault backup: 2024-10-18 19:23:56

--- quartz/components/scripts/graph.inline.ts
+++ quartz/components/scripts/graph.inline.ts
@@ -233,11 +233,11 @@ async function renderGraph(container: string, fullSlug: FullSlug) {
       return -1;
     }
   
-    if (d.text === "Computer Vision") {
+    if (d.text === "Computer Vision" || "Machine Learning") {
       return baseRadius;
     }
     // Find the CV node
-    const cvNode = graphData.nodes.find(node => node.text === "Computer Vision");
+    const cvNode = graphData.nodes.find(node => node.text === "Computer Vision" || "Machine Learning");
     if (!cvNode) return 5;
   
     // Calculate depth from "CV" node
