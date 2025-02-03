7bbf5187c1bdbb51dd4f75f5fd58d1f90e715ba1
Byungha Ko <rhqo123@gmail.com>
2025-02-03 11:42:10 +0900

    vault backup: 2025-02-03 11:42:10

--- functions.ts
+++ functions.ts
@@ -3,13 +3,13 @@ import { Options } from "./quartz/components/ExplorerNode"
 export const mapFn: Options["mapFn"] = (node) => {
   if (node.depth > 0) {
     if (node.file) {
-      node.displayName = "📄 " + node.displayName
+      node.displayName = "❍ " + node.displayName
     } 
   }
 }
 
 export const filterFn: Options["filterFn"] = (node) => {
   // set containing names of everything you want to filter out
-  const omit = new Set(["title"])
+  const omit = new Set(["title", "index", "404"])
   return !omit.has(node.name.toLowerCase())
 }
