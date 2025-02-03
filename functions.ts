9cd0624d416f9f52d6c18b78cca237380f344c09
Byungha Ko <rhqo123@gmail.com>
2025-02-03 11:32:10 +0900

    vault backup: 2025-02-03 11:32:10

--- functions.ts
+++ functions.ts
@@ -2,17 +2,14 @@ import { Options } from "./quartz/components/ExplorerNode"
 
 export const mapFn: Options["mapFn"] = (node) => {
   if (node.depth > 0) {
-    // set emoji for file/folder
     if (node.file) {
       node.displayName = "📄 " + node.displayName
-    } else {
-      node.displayName = "📁 " + node.displayName
-    }
+    } 
   }
 }
 
 export const filterFn: Options["filterFn"] = (node) => {
   // set containing names of everything you want to filter out
-  const omit = new Set(["authoring content", "tags", "hosting"])
+  const omit = new Set(["title"])
   return !omit.has(node.name.toLowerCase())
 }
