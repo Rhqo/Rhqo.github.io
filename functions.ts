6c737073114b00e6f9d5501df6e10ac96a47cd6c
Byungha Ko <rhqo123@gmail.com>
2025-02-03 11:02:11 +0900

    vault backup: 2025-02-03 11:02:11

---
+++ functions.ts
@@ -0,0 +1,10 @@
+import { Options } from "./quartz/components/ExplorerNode"
+export const mapFn: Options["mapFn"] = (node) => {
+  // implement your function here
+}
+export const filterFn: Options["filterFn"] = (node) => {
+  // implement your function here
+}
+export const sortFn: Options["sortFn"] = (a, b) => {
+  // implement your function here
+}
